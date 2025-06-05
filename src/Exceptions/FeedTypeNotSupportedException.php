31f9a2ebaa3e4c606e25554c2bca4f7b8ffb9f04
Maciej Kubień <mkubien@gmail.com>
2025-06-05 21:20:26 +0200

    PHP 8.4 compatibility

--- src/Exceptions/FeedTypeNotSupportedException.php
+++ src/Exceptions/FeedTypeNotSupportedException.php
@@ -8,7 +8,7 @@ use Exception;
 
 class FeedTypeNotSupportedException extends Exception
 {
-    public function __construct(string $message, int $code = 0, Exception $previous = null)
+    public function __construct(string $message, int $code = 0, ?Exception $previous = null)
     {
         Exception::__construct($message, $code, $previous);
     }
